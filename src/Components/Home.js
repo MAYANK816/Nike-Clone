9cba28715d4da142df5a0a7d31bbc2312693e856
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2023-01-04 11:58:36 +0530

    Bug fixing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,61 +12,62 @@ useEffect(() => {
 }, []);
 
   return (
-    <div className="Home_Body">
-    <div className='Home_Courses'>
-      <div className='home_circle'></div>    
-      <div className='Home_Courses_Card'>
-      <p>Summer Collections <span>2023</span></p>
-      </div>
-      <div className="Home_Courses_Card">
-      <img src="/Images/product3.png" alt="summer"/>
-      <div className='home_circle_2'></div>   
-      </div>
-    </div>
-    <h3 className="Home_Courses_Title">Popular <span>Products</span></h3>
+    <></>
+    // <div className="Home_Body">
+    // <div className='Home_Courses'>
+    //   <div className='home_circle'></div>    
+    //   <div className='Home_Courses_Card'>
+    //   <p>Summer Collections <span>2023</span></p>
+    //   </div>
+    //   <div className="Home_Courses_Card">
+    //   <img src="/Images/product3.png" alt="summer"/>
+    //   <div className='home_circle_2'></div>   
+    //   </div>
+    // </div>
+    // <h3 className="Home_Courses_Title">Popular <span>Products</span></h3>
 
-    <div className='Home_Products'>    
-          {/* <CardComponent
-          key="1"
-          courseName="Nike Air Jordan-100"
-          id="product_1"
-          rating=" (4.5)"
-          price="$20.20"
-          image="/Images/product1.jpg"
-          /> */}
+    // <div className='Home_Products'>    
+    //       {/* <CardComponent
+    //       key="1"
+    //       courseName="Nike Air Jordan-100"
+    //       id="product_1"
+    //       rating=" (4.5)"
+    //       price="$20.20"
+    //       image="/Images/product1.jpg"
+    //       /> */}
 
-          {/* <CardComponent
-          key="2"
-          courseName="Nike Air Jordan-100"
-          id="product_2"
-          rating=" (4.5)"
-          price="$40.20"
-          image="/Images/product2.jpg"
-          /> */}
-        {/* <div className='home_circle'></div>    
-          <CardComponent
-          key="3"
-          courseName="Nike Air Jordan-100"
-          id="product_3"
-          rating=" (4.5)"
-          price="$50.20"
-          image="/Images/product1.jpg"
-          /> */}
-          {/* <CardComponent
-          key="4"
-          courseName="Nike Air Jordan-100"
-          id="product_3"
-          rating=" (4.5)"
-          price="$70.20"
-          image="/Images/product3.png"
-          /> */}
-           <div className='home_circle_2'></div>   
-          </div>
-          <Footer/>
-          <div className='Home_Footer_Container_5'>
-            <p>© 2023 All Rights Reserved. Designed by <span>Mayank</span></p>
-        </div>
-            </div>
+    //       {/* <CardComponent
+    //       key="2"
+    //       courseName="Nike Air Jordan-100"
+    //       id="product_2"
+    //       rating=" (4.5)"
+    //       price="$40.20"
+    //       image="/Images/product2.jpg"
+    //       /> */}
+    //     {/* <div className='home_circle'></div>    
+    //       <CardComponent
+    //       key="3"
+    //       courseName="Nike Air Jordan-100"
+    //       id="product_3"
+    //       rating=" (4.5)"
+    //       price="$50.20"
+    //       image="/Images/product1.jpg"
+    //       /> */}
+    //       {/* <CardComponent
+    //       key="4"
+    //       courseName="Nike Air Jordan-100"
+    //       id="product_3"
+    //       rating=" (4.5)"
+    //       price="$70.20"
+    //       image="/Images/product3.png"
+    //       /> */}
+    //        <div className='home_circle_2'></div>   
+    //       </div>
+    //       <Footer/>
+    //       <div className='Home_Footer_Container_5'>
+    //         <p>© 2023 All Rights Reserved. Designed by <span>Mayank</span></p>
+    //     </div>
+    //         </div>
   )
 }
 

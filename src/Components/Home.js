4796f935e260ec2eb99d7a4cb4bbe6578f7fd3f2
MAYANK816 <mayank0218.cse19@chitkara.edu.in>
2023-01-04 12:20:55 +0530

    Bug fixing

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,20 +12,18 @@ useEffect(() => {
 }, []);
 
   return (
-   
-    // <div className="Home_Body">
-    // <div className='Home_Courses'>
-    //   <div className='home_circle'></div>    
-    //   <div className='Home_Courses_Card'>
-    //   <p>Summer Collections <span>2023</span></p>
-    //   </div>
-    //   <div className="Home_Courses_Card">
-    //   <img src="/Images/product3.png" alt="summer"/>
-    //   <div className='home_circle_2'></div>   
-    //   </div>
-    // </div>
-    // <h3 className="Home_Courses_Title">Popular <span>Products</span></h3>
-<>
+    <div className="Home_Body">
+    <div className='Home_Courses'>
+      {/* <div className='home_circle'></div>     */}
+      <div className='Home_Courses_Card'>
+      <p>Summer Collections <span>2023</span></p>
+      </div>
+      <div className="Home_Courses_Card">
+      <img src="/Images/product3.png" alt="summer"/>
+      {/* <div className='home_circle_2'></div>    */}
+      </div>
+    </div>
+    <h3 className="Home_Courses_Title">Popular <span>Products</span></h3>
     <div className='Home_Products'>    
           <CardComponent
           key="1"
@@ -44,7 +42,7 @@ useEffect(() => {
           price="$40.20"
           image="/Images/product2.jpg"
           />
-        <div className='home_circle'></div>    
+        {/* <div className='home_circle'></div>     */}
           <CardComponent
           key="3"
           courseName="Nike Air Jordan-100"
@@ -61,13 +59,13 @@ useEffect(() => {
           price="$70.20"
           image="/Images/product3.png"
           />
-           <div className='home_circle_2'></div>   
+           {/* <div className='home_circle_2'></div>    */}
           </div>
           <Footer/>
           <div className='Home_Footer_Container_5'>
             <p>© 2023 All Rights Reserved. Designed by <span>Mayank</span></p>
         </div>
-        </>
+        </div>
   )
 }
 
